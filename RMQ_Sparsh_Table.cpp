c45524836897a5560aa2d2f285133cf606834168
aakash <aakashchaudhary172m@gmail.com>
2023-07-04 19:00:48 +0530

    added Sparsh Table

---
+++ RMQ_Sparsh_Table.cpp
@@ -0,0 +1,41 @@
+/**
+ *           author: aakash_172
+ *           created: 2023-07-04    18:57:40
+ **/
+#include <bits/stdc++.h>
+
+template <typename T>
+struct RMQ
+{
+    vector<vector<T>> spt;
+
+    RMQ(const vector<T> &a) : spt(1, a)
+    {
+        int n = (int)a.size();
+        for (int j = 1; 1 << j <= n; j++)
+        {
+            spt.emplace_back(n - (1 << j) + 1);
+            for (int i = 0; i + (1 << j) <= n; i++)
+                spt[j][i] = min(spt[j - 1][i], spt[j - 1][i + (1 << (j - 1))]);
+        }
+    }
+
+    T query(int i, int j)
+    {
+        int k = __lg(j - i + 1);
+        return min(spt[k][i], spt[k][j - (1 << k) + 1]);
+    }
+};
+
+using namespace std;
+
+int main()
+{
+    vector<int> a;
+    // Declare
+    //   RMQ<int> m1(a);
+
+    // query
+    //  RMQ<int> m1(a);
+    return 0;
+}
