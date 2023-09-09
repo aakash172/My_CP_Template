58fc879f88f4b3ee0ae9aa31894f5882362c4e07
aakash <aakashchaudhary172m@gmail.com>
2023-09-09 11:23:24 +0530

    bfs update

--- Tree+Graph/Bfs_Tree.cpp
+++ Tree+Graph/Bfs_Tree.cpp
@@ -23,6 +23,38 @@ void bfs(ll sr, vector<ll> adj[], vector<ll> &dist)
     }
 }
 
+void bfs(ll x, ll y, vector<vector<ll>> &dist, vector<vector<ll>> &mat)
+{
+    int n = dist.size(), m = dist[0].size();
+    queue<pair<ll, ll>> q;
+    q.push({x, y});
+    dist[x][y] = 0;
+    while (!q.empty())
+    {
+        auto x = q.front().first;
+        auto y = q.front().second;
+        q.pop();
+
+        vector<pair<int, int>> move = {{1, 0},
+                                       {0, 1},
+                                       {-1, 0},
+                                       {0, -1}};
+
+        for (auto &mv : move)
+        {
+            int i = x + mv.first;
+            int j = y + mv.second;
+            if (i > n || j > m || j < 1 || i < 1)
+                continue;
+            if (dist[i][j] > dist[x][y] + 1)
+            {
+                dist[i][j] = dist[x][y] + 1;
+                q.push({i, j});
+            }
+        }
+    }
+}
+
 int main()
 {
 
